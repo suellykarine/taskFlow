d47e3b4ce667e9439a709491fa9ebc727dc97b0c
Suélly Araujo <suelly.araujo@BSATech.LOCAL>
2025-06-17 16:56:53 -0300

    ajustes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,8 @@
 export const Header = () => {
-  const handleReportClick = () => {
-    alert("Funcionalidade de gerar relatório a ser implementada!");
-  };
-
   return (
     <header>
       <h1 className="gradient-text">TaskFlow</h1>
-      <button className="report-button" onClick={handleReportClick}>
-        Relatório
-      </button>
+      <button className="report-button">Relatório</button>
     </header>
   );
 };
